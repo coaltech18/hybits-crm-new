1a1820de39972294f8b5e3f44a2f936cc85b91b9
surface book 2 hemanth mahesh <coaltech18@gmail.com>
2025-09-19 03:16:55 +0530

    new verison of the crm with the typescript instead of the javascript.

--- src/types/forms.ts
+++ src/types/forms.ts
@@ -245,7 +245,7 @@ export interface FormProps<T> {
   children: React.ReactNode;
 }
 
-export interface FormFieldProps {
+export interface FormFieldComponentProps {
   field: FormField;
   value: any;
   error?: string;
